d2cb6895991db24dd5463820e6771ba22e60bb0a
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-04-04 18:21:53 +0900

    Run tests parallelly

---
+++ tests/load_balancer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import argparse
+import pathlib
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Categorize and list test files in the tests/ directory')
+    parser.add_argument('keyword', choices=('stable', 'unstable'))
+    args = parser.parse_args()
+
+    tests = pathlib.Path('tests')
+    unstable = [
+        tests / 'service_codeforces.py',
+        tests / 'service_codechef.py',
+    ]
+
+    if args.keyword == 'unstable':
+        files = unstable
+    elif args.keyword == 'stable':
+        files = [file for file in tests.glob('*.py') if file not in unstable]
+    else:
+        assert False
+
+    print('::set-output name=files::', *map(str, files))
+
+
+if __name__ == '__main__':
+    main()
