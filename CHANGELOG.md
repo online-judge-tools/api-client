127e1b89dc251a995ff4f3b2bc73b279c4836ff5
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-05-02 02:30:22 +0900

    RELEASE: 10.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Change Log
 
-## 10.0.0 / Unreleased
+## 10.0.0 / 2020-05-02
 
--   forked from [online-judge-tools](https://github.com/kmyk/online-judge-tools) `v9.2.0`
+-   forked from [online-judge-tools](https://github.com/kmyk/online-judge-tools) `v9.2.2`
 -   add `oj-api` command
--- onlinejudge/__about__.py
+++ onlinejudge/__about__.py
@@ -1,9 +1,9 @@
 # Python Version: 3.x
-__package_name__ = 'online-judge-tools'
+__package_name__ = 'online-judge-api-client'
 __author__ = 'Kimiyuki Onaka'
 __email__ = 'kimiyuki95@gmail.com'
 __license__ = 'MIT License'
-__url__ = 'https://github.com/kmyk/online-judge-tools'
-__version_info__ = (9, 2, 2, 'final', 0)
+__url__ = 'https://github.com/online-judge-tools/api-client'
+__version_info__ = (10, 0, 0, 'final', 0)
 __version__ = '.'.join(map(str, __version_info__[:3]))
-__description__ = 'Tools for online-judge services'
+__description__ = 'API client to develop tools for competitive programming'
