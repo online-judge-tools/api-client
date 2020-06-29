c44ab05062ed8f19906ad0427d92193076b6db33
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-06-30 05:28:28 +0900

    Update setup.cfg and setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
     install_requires=[
         'appdirs >= 1',
         'beautifulsoup4 >= 4',
-        'colorama >= 0.3',
         'colorlog >= 4.1.0',
         'lxml >= 4',
         'requests >= 2',
